52ad228aa33d5815dee08d370b2cf1d3edcfd93f
Kenneth Baker <spyksaturn@gmail.com>
2023-02-28 15:00:29 -0500

    initial commit

---
+++ FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzChallenge
+{
+    public static class FizzBuzz
+
+    {
+        public static string FizzOrBuzz(int a)
+        {
+            if (a % 3 == 0 && a % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (a % 5 == 0)
+            {
+                return "Buzz";
+            }
+                return "Fizz";
+            
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzzChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = FizzBuzz.FizzOrBuzz(5);
+            Console.WriteLine(answer);
+        }
+    }
+}
